aa0bda07c8c101f2fa62797c3dfc32e70fad7d58
kgalarza95 <kevin_galata@hotmail.com>
2025-10-27 10:44:27 -0500

    clear layout not use

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -15,16 +15,8 @@ import { FUSE_VERSION } from '@fuse/version';
 import { Subject, combineLatest, filter, map, takeUntil } from 'rxjs';
 import { SettingsComponent } from './common/settings/settings.component';
 import { EmptyLayoutComponent } from './layouts/empty/empty.component';
-import { CenteredLayoutComponent } from './layouts/horizontal/centered/centered.component';
-import { EnterpriseLayoutComponent } from './layouts/horizontal/enterprise/enterprise.component';
-import { MaterialLayoutComponent } from './layouts/horizontal/material/material.component';
-import { ModernLayoutComponent } from './layouts/horizontal/modern/modern.component';
 import { ClassicLayoutComponent } from './layouts/vertical/classic/classic.component';
 import { ClassyLayoutComponent } from './layouts/vertical/classy/classy.component';
-import { CompactLayoutComponent } from './layouts/vertical/compact/compact.component';
-import { DenseLayoutComponent } from './layouts/vertical/dense/dense.component';
-import { FuturisticLayoutComponent } from './layouts/vertical/futuristic/futuristic.component';
-import { ThinLayoutComponent } from './layouts/vertical/thin/thin.component';
 
 @Component({
     selector: 'layout',
@@ -33,16 +25,8 @@ import { ThinLayoutComponent } from './layouts/vertical/thin/thin.component';
     encapsulation: ViewEncapsulation.None,
     imports: [
         EmptyLayoutComponent,
-        CenteredLayoutComponent,
-        EnterpriseLayoutComponent,
-        MaterialLayoutComponent,
-        ModernLayoutComponent,
         ClassicLayoutComponent,
         ClassyLayoutComponent,
-        CompactLayoutComponent,
-        DenseLayoutComponent,
-        FuturisticLayoutComponent,
-        ThinLayoutComponent,
         SettingsComponent,
     ],
 })
